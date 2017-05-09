01028e9f68868f16495b75c7b59bdc6bb9417e92
Lai Chun Pong <laichunpongben@gmail.com>
2017-05-09 22:59:16 +0800

    Add logger

--- quandl_data.py
+++ quandl_data.py
@@ -1,26 +1,31 @@
 import quandl
 import matplotlib
 import matplotlib.pyplot as plt
+import logging
+
+logger = logging.getLogger('quant')
 
 
 class QuandlData(object):
     def __init__(self, ticker):
         self.ticker = ticker
-        self.df = self.get_df()
+        self.df = self.request_df()
 
-    def get_df(self):
+    def request_df(self):
         try:
             return quandl.get(self.ticker)
         except Exception as e:
-            print(e)
+            logger.exception('Failed to download data')
             return None
 
     def plot(self):
-        matplotlib.style.use('ggplot')
-        self.df.plot()
-        plt.title(self.ticker)
-        plt.show()
-
+        if self.df is not None:
+            matplotlib.style.use('ggplot')
+            self.df.plot()
+            plt.title(self.ticker)
+            plt.show()
+        else:
+            logger.error('No data available')
 
 if __name__ == '__main__':
     ticker = 'CBOE/VIX'
